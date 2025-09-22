1da70233e6e5daa76fc72f367734f16e482b9284
Luciana Quirino <lucianaq17@gmail.com>
2025-09-22 22:50:41 +0000

    CartaNew

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,11 +4,18 @@ int main() {
 
   //Carta1
   char estado1[50];
+  char codigo1[50];
+  char cidade1[50];
 
   printf("Estado: ",estado1);
   scanf("%s",&estado1);
 
+  printf("Cidade: ",cidade1);
+  scanf("%s",&,cidade1);
+
+
   printf("Estado: %s\n",estado1);
+  printf("Cidade: ")
 
 
 
