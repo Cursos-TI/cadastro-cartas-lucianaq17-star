2a176f9c7af155ed32071c5a6681a28b9158dd56
Luciana Quirino <lucianaq17@gmail.com>
2025-09-22 23:30:14 +0000

    Incluir

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,22 +3,26 @@
 int main() {
 
   char estado1[50];
-  char codigo1[50];
+  //char codigo1[50];
   //char cidade1[50];
+  int populacao1;
 
   printf("Estado: ");
   scanf("%s",&estado1);
 
-  print("Código: ");
-  scanf("%s",&codigo1);
+  //print("Código: ");
+  //scanf("%s",&codigo1);
 
   //printf("Cidade: ");
   //scanf("%s",&cidade1);
 
+  printf("População: ");
+  scanf("%d",&populacao1);
 
   printf("Estado: %s\n",estado1);
-  printf("Código: %s\n",codigo1);
+  //printf("Código: %s\n",codigo1);
   //printf("Cidade: %s\n",cidade1);
+  printf("Populção: %d\n");
 
 
 
