88c31878a1fe6eb80e97c769c81cd6bd24b9b879
Luciana Quirino <lucianaq17@gmail.com>
2025-09-23 23:08:20 +0000

    Alteracao

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,26 +3,31 @@
 int main() {
 
   char estado1[50];
-  //char codigo1[50];
   //char cidade1[50];
-  int populacao1;
+  int populacao1, municipios1
+  float pib1; area1;
+
 
   printf("Estado: ");
   scanf("%s",&estado1);
 
-  //print("Código: ");
-  //scanf("%s",&codigo1);
+  print("Área: ");
+  scanf("%f",&area1);
 
-  //printf("Cidade: ");
-  //scanf("%s",&cidade1);
+  printf("Pib: ");
+  scanf("%s",&pib1);
 
   printf("População: ");
   scanf("%d",&populacao1);
 
+  printf("Municípios: ");
+  scanf("%d",&municipios1);
+
   printf("Estado: %s\n",estado1);
-  //printf("Código: %s\n",codigo1);
+  printf("Área: %f\n",area1);
   //printf("Cidade: %s\n",cidade1);
-  printf("Populção: %d\n",populacao1);
+  printf("População: %d\n",populacao1);
+  printf("Municípios: %d\n",municipios1);
 
 
 
