90ded126d25794214272718997d99acc39a1e659
Luciana Quirino <lucianaq17@gmail.com>
2025-09-24 07:01:48 -0300

    Inclusão carta 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,10 +2,11 @@
 
 int main() {
   
-char estado1 [50];
-int populacao1, municipios1;
-float pibl, areal;
+char estado1 [50], estado2[50];
+int populacao1, municipios1,populacao2, municipios2;
+float pibl, areal, pib2, area2;
 
+//Entrada dados carta 1
 printf("Estado: ");
 scanf("%s", estado1);
 
@@ -21,11 +22,36 @@ scanf("%d", &populacao1);
 printf("Municípios: ");
 scanf("%d", &municipios1);
 
+//Entrada dados carta 2
+printf("Estado: ");
+scanf("%s", estado2);
+
+printf("Área: ");
+scanf("%f",&area2);
+
+printf("Pib: ");
+scanf("%f",&pib2);
+
+printf("População: ");
+scanf("%d", &populacao2);
+
+printf("Municípios: ");
+scanf("%d", &municipios2);
+
+// Saída carta 1
 printf("Estado: %s\n", estado1);
 printf("Área: %f\n",area1);
 printf("Pib: %f\n",pib1);
 printf("População: %d\n", populacao1);
 printf("Municípios: %d\n", municipios1);
+
+//Saída carta 2
+printf("Estado: %s\n", estado2);
+printf("Área: %f\n",area2);
+printf("Pib: %f\n",pib2);
+printf("População: %d\n", populacao2);
+printf("Municípios: %d\n", municipios2);
+
 
 return 0;
 
